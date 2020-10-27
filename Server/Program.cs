a2cdaac28efc9751458c4e0b862a946da39d0d2a
ed <eramos34@student.rccd.edu>
2020-10-26 23:44:44 -0700

    server and client done

---
+++ Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace Server
+{
+    class Program
+    {           //https://youtu.be/g5yEWLJxNmI?t=619
+        static void Main(string[] args)
+        {
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 1309);
+            listener.Start();
+            while(true)
+            {
+                Console.WriteLine("waiting for connections");
+                TcpClient client = listener.AcceptTcpClient();
+                Console.WriteLine("client accepted.");
+                NetworkStream stream = client.GetStream();
+                StreamReader reader = new StreamReader(client.GetStream());
+                StreamWriter writer = new StreamWriter(client.GetStream());
+            try
+            {
+                    byte[] buffer = new byte[1024];
+                    stream.Read(buffer, 0, buffer.Length);
+                    int recv = 0;
+                    foreach (byte b in buffer)
+                    {
+                        if(b!=0)
+                        {
+                            recv++;
+                        }
+                    }
+                    string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                    Console.WriteLine("request receive: " + request);
+                    writer.WriteLine("done");
+                    writer.Flush();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            }
+           
+        }
+    }
+}
--- TCP-Socket-Tutorial/Program.cs
+++ TCP-Socket-Tutorial/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
 
 namespace TCP_Socket_Tutorial
 {
@@ -6,7 +10,37 @@ namespace TCP_Socket_Tutorial
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //https://www.youtube.com/watch?v=g5yEWLJxNmI
+            try
+            {
+                TcpClient client = new TcpClient("127.0.0.1", 1309);
+                string message = "my name is edmar";
+
+                int bytetocount = Encoding.ASCII.GetByteCount(message + 1);
+                byte[] sendData = new byte[bytetocount];
+
+                sendData = Encoding.ASCII.GetBytes(message);
+                NetworkStream stream = client.GetStream();
+                stream.Write(sendData, 0, sendData.Length);
+                Console.WriteLine("sending data...");
+
+                StreamReader reader = new StreamReader(stream);
+                string response = reader.ReadLine();
+                Console.WriteLine(response);
+
+                stream.Close();
+                client.Close();
+
+
+
+
+
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
